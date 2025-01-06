f06120a707e7b51960d3400cd357b8fd863a186b
GabrielLimab <galimabarros@gmail.com>
2025-01-06 10:03:05 -0300

    Update

--- server/api_rest.py
+++ server/api_rest.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import datetime
 import pickle
 
 app = Flask(__name__)
@@ -9,11 +10,12 @@ RULES_PATH = "/app/shared_volume/rules.pickle"
 with open(RULES_PATH, 'rb') as f:
     rules = pickle.load(f)
 
+def get_date():
+    return datetime.datetime.now().isoformat()
+
 @app.route('/api/recommend', methods=['POST'])
 def recommend():
-    print("DEBUG: Request received")
     data = request.get_json(force=True)
-    print("DEBUG: Request data: ", data)
     liked_songs = data['songs']
     recommendationsRules = [list(rule[1]) for rule in rules if set(liked_songs).intersection(rule[0])]
     recommendations = list(set([song for sublist in recommendationsRules for song in sublist]))
@@ -21,7 +23,7 @@ def recommend():
     response = {
         "songs": recommendations,
         "version": "1.0",
-        "model_date": "2025-01-05"
+        "model_date": get_date()
     }
 
     return jsonify(response)
